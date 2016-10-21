9ef4437023fd654820fa75de0c2a80db66b851c2
Sam Weston <sam.weston@snowflakesoftware.com>
2016-10-21 15:54:31 +0100

    Pushing now pushes a particular file

--- 3scalelogin.py
+++ 3scalelogin.py
@@ -48,6 +48,7 @@ def pull():
     map_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/map_file"), 'w+')
     for match in matches:
         match_url = "https://" + THREESCALE_ACCOUNT + ".3scale.net/admin/api_docs/services/" + match.group(1) + ".json"
+        print("Downloading " + match.group(3) + ".json")
         file_content = session_requests.get(match_url).content
         json_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/" + match.group(3) + ".json"), 'wb')
         json_file.write(file_content)
@@ -58,11 +59,13 @@ def pull():
     map_file.close()
 
 
-def push():
+def push(filename):
+    if not filename or ".json" not in filename:
+        print("Please enter a filename ending in '.json' to push.")
     map_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/map_file"), 'r')
     for map_file_line in map_file:
         map_list = map_file_line.split(',')
-        if map_list[0] == "test.json":
+        if map_list[0] == filename:
             json_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/" + map_list[0]), 'r')
             json_contents = {"body": json_file.read()}
             api_url = 'https://' + THREESCALE_ACCOUNT + '.3scale.net/admin/api/active_docs/' + map_list[1].rstrip() + \
@@ -75,9 +78,10 @@ def push():
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process CLI Arguments')
     parser.add_argument('command', choices=['pull', 'push'])
+    parser.add_argument('--filename', required=False)
     args = parser.parse_args()
 
     if args.command == "pull":
         pull()
     elif args.command == "push":
-        push()
+        push(args.filename)
