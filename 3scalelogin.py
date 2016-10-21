1b932b2091fb511e3ebf23e2247c5872ef005ca0
Sam Weston <sam.weston@snowflakesoftware.com>
2016-10-21 15:45:39 +0100

    Pushing now works but locked down to test.json until I get confirmation working

--- 3scalelogin.py
+++ 3scalelogin.py
@@ -11,6 +11,7 @@ config.read("config.ini")
 USERNAME = config['3scale']['username']
 PASSWORD = config['3scale']['password']
 THREESCALE_ACCOUNT = config['3scale']['account']
+THREESCALE_ACCESS_TOKEN = config['3scale']['access_token']
 ACTIVEDOCS_PATH = config['3scale']['path']
 
 LOGIN_URL = "https://" + THREESCALE_ACCOUNT + ".3scale.net/p/login"
@@ -41,14 +42,35 @@ def pull():
     # Scrape url
     result = session_requests.get(URL, headers=dict(referer=URL))
     html_string = result.content.decode('utf-8')
-    matches = re.findall('\/admin\/api_docs\/services\/(\d+)\/preview', html_string)
+    regex = re.compile('/api_docs/services/(\d+)/preview" title="Preview service spec">([^<]+)</a></td><td>([^<]+)')
+    matches = regex.finditer(html_string)
 
-    for id in matches:
-        file_content = session_requests.get("https://" + THREESCALE_ACCOUNT + ".3scale.net/admin/api_docs/services/" + id +
-                                   ".json").content
-        f = open(os.path.expanduser(ACTIVEDOCS_PATH + "/" + id + ".json"), 'wb')
-        f.write(file_content)
-        f.close()
+    map_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/map_file"), 'w+')
+    for match in matches:
+        match_url = "https://" + THREESCALE_ACCOUNT + ".3scale.net/admin/api_docs/services/" + match.group(1) + ".json"
+        file_content = session_requests.get(match_url).content
+        json_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/" + match.group(3) + ".json"), 'wb')
+        json_file.write(file_content)
+        json_file.close()
+
+        map_file.write(match.group(3) + ".json" + "," + match.group(1) + "\n")
+
+    map_file.close()
+
+
+def push():
+    map_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/map_file"), 'r')
+    for map_file_line in map_file:
+        map_list = map_file_line.split(',')
+        if map_list[0] == "test.json":
+            json_file = open(os.path.expanduser(ACTIVEDOCS_PATH + "/" + map_list[0]), 'r')
+            json_contents = {"body": json_file.read()}
+            api_url = 'https://' + THREESCALE_ACCOUNT + '.3scale.net/admin/api/active_docs/' + map_list[1].rstrip() + \
+                      '.xml?access_token=' + THREESCALE_ACCESS_TOKEN
+            print("Pushing " + map_list[0] + " to " + api_url)
+            put_request = requests.put(api_url, json_contents)
+            print(put_request.status_code)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process CLI Arguments')
@@ -58,5 +80,4 @@ if __name__ == '__main__':
     if args.command == "pull":
         pull()
     elif args.command == "push":
-        print("Push time")
-
+        push()
